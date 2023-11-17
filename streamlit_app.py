a9b24697dfc84e8211a82c0106063b2a82ad9759
st7620 <soniatan127@gmail.com>
2023-11-17 01:54:00 -0500

    Updated streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,26 +13,11 @@ st.set_page_config(page_title="Document Chatbot")
 # File uploader widget  
 uploaded_files = st.file_uploader("Choose your files", accept_multiple_files=True)
 
-# Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
-
-# Display chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
-
-# User-provided prompt
-if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
-
-# Create textractor model
-textractor = Textractor()
-
 # Check if there are any uploaded files
 if uploaded_files:
+    # Create textractor model
+    textractor = Textractor()
+    
     data = []
     for file in uploaded_files:
 
@@ -58,6 +43,9 @@ if uploaded_files:
     # Create an index for the list of text
     embeddings.index(data)
 
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
 
 # Function for generating response
 def generate_response(prompt_input):                       
@@ -65,6 +53,11 @@ def generate_response(prompt_input):
     question_set = {"context": context, "question": prompt_input}
     return nlp(question_set)["answer"]
 
+# User-provided prompt
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
 
 # Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
