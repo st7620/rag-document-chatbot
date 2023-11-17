97b2962ae7891f136ffb67070bebe38e8f80721c
st7620 <soniatan127@gmail.com>
2023-11-17 02:20:44 -0500

    Fixed chat message function

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from txtai import Embeddings
 import nltk
-nltk.download('punkt')
 from txtai.pipeline import Textractor
 import tempfile
 import os
@@ -13,26 +12,12 @@ st.set_page_config(page_title="Document Chatbot")
 # File uploader widget  
 uploaded_files = st.file_uploader("Choose your files", accept_multiple_files=True)
 
-# Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
-
-# Function for generating response
-def generate_response(prompt_input):                       
-    context = embeddings.search(prompt_input, 1)[0]["text"]
-    question_set = {"context": context, "question": prompt_input}
-    return nlp(question_set)["answer"]
-
-# User-provided prompt
-if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
-
 # Check if there are any uploaded files
 if uploaded_files:
     # Create textractor model
     textractor = Textractor()
+
+    nltk.download('punkt')
     
     data = []
     for file in uploaded_files:
@@ -58,6 +43,27 @@ if uploaded_files:
 
     # Create an index for the list of text
     embeddings.index(data)
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
+
+# Display chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# Function for generating response
+def generate_response(prompt_input):                       
+    context = embeddings.search(prompt_input, 1)[0]["text"]
+    question_set = {"context": context, "question": prompt_input}
+    return nlp(question_set)["answer"]
+
+# User-provided prompt
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
 
 # Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
